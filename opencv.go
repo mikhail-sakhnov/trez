ae138ad3bbbbae3c3bcab83ab2b1d992ed6baf9c
btsyrendylykov <bator.tsyrendylykov@lazada.com>
2017-11-16 15:21:35 +0300

    Fix image quality parameter

--- opencv.go
+++ opencv.go
@@ -108,6 +108,17 @@ func resize(src *C.IplImage, options Options) (*ProcessResult, error) {
 	}
 	options.Width, options.Height = calcNewSize(options)
 
+	// Check quality range
+	if options.Quality < 0 {
+		options.Quality = 0
+	}
+	if options.Quality > 100 {
+		options.Quality = 100
+	}
+	if options.Quality == 0 {
+		options.Quality = 85	// default value
+	}
+
 	// Get the size of the desired output image
 	size := C.cvSize(C.int(options.Width), C.int(options.Height))
 
@@ -219,41 +230,56 @@ func resize(src *C.IplImage, options Options) (*ProcessResult, error) {
 		C.cvResetImageROI(mid)
 	}
 
-	// set default compression
-	if options.Quality == 0 {
-		options.Quality = 95
-	}
-	var compression [3]C.int
+	var params [6]C.int
 	var ext *C.char
 	switch options.Format {
 	case JPEG:
-		modificator := C.CV_IMWRITE_JPEG_QUALITY
-		if options.Progressive {
-			modificator = C.CV_IMWRITE_JPEG_PROGRESSIVE
-		}
 		ext = C.CString(".jpg")
-		compression = [3]C.int{
-			C.int(modificator),
-			C.int(options.Quality),
+		params = [6]C.int{
+			C.CV_IMWRITE_JPEG_QUALITY,
+			C.int(options.Quality),	// from 0 to 100 (the higher is the better). Default value is 95.
 			0,
+			0,
+			0,
+			0,
+		}
+		if options.Progressive {
+			params[3] = C.CV_IMWRITE_JPEG_PROGRESSIVE
 		}
 	case WEBP:
 		ext = C.CString(".webp")
-		compression = [3]C.int{
+		// from 1 to 100 (the higher is the better).
+		// By default (without any parameter) and for quality above 100 the lossless compression is used.
+		q := options.Quality
+		if q == 0 {
+			q = 1
+		}
+		params = [6]C.int{
 			C.CV_IMWRITE_WEBP_QUALITY,
-			C.int(options.Quality),
+			C.int(q),
+			0,
+			0,
+			0,
 			0,
 		}
 	case PNG:
 		ext = C.CString(".png")
-		compression = [3]C.int{
-			C.CV_IMWRITE_PNG_STRATEGY_DEFAULT,
-			C.int(options.Quality),
+		// from 0 to 9. A higher value means a smaller size and longer compression time. Default value is 3.
+		q := (100 - options.Quality) / 10
+		if q > 9 {
+			q = 9
+		}
+		params = [6]C.int{
+			C.CV_IMWRITE_PNG_COMPRESSION,
+			C.int(q),
+			0,
+			0,
+			0,
 			0,
 		}
 	}
 	// encode
-	ret := C.cvEncodeImage(ext, unsafe.Pointer(dst), &compression[0])
+	ret := C.cvEncodeImage(ext, unsafe.Pointer(dst), &params[0])
 	C.free(unsafe.Pointer(ext))
 
 	if ret == nil {
